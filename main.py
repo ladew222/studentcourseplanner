0815f44a5dd2acdc0b9b601d143dd462791841ea
alelopergolo <aloper75113@viterbo.edu>
2023-04-12 18:18:21 -0500

    changes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from people import Person, Student, Teacher
-from schedule import CourseSchedule
+from schedule import CourseSchedule, Time, Timeslot
 def main():
     # Create instances of the classes
     p = Person("John", 30, "123 Main St.")
     s = Student("Jane", 20, "456 Maple Ave.", "S1234", ["Math", "Science"])
     t = Teacher("Dr. Smith", 45, "789 Oak St.", "T5678", "English")
+    # Create instances of Time, Instructor, and Timeslot
+    time1 = Time("9:00am", "10:30am")
+    timeslot1 = Timeslot("Monday", "9:00am - 10:30am")
 
     # Print out the attributes of the instances
     print("Person:", p.name, p.age, p.address)
