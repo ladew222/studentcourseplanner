78674328d7b8710383b6fb4750e63a0191c2fa84
sarrodri <114839018+sarrodri@users.noreply.github.com>
2023-04-13 11:32:59 -0500

    reading in student plans csv

--- schedule.py
+++ schedule.py
@@ -5,33 +5,24 @@ class Course:
         self.course_name = course_name
         self.course_id = course_id
 
-class PlannedCourse(Course):
+class PlannedCourse:
     def __init__(self, course_name, course_id, year, semester):
         # Initialize a new planned course object with a name, id, year and semester.
         # This object inherits the name and id from our course class.
         super().__init__(course_name, course_id)
         self.year = year
         self.semester = semester
-
+    
 class ScheduledClass:
     def __init__(self, time, instructor, timeslot, capacity, year, section, semester):
         # Initialize a new scheduled class object with a time, instructor, timeslot, capacity, year, section and semester.
-        self.time = Time
+        self.time = time
         self.instructor = instructor
-        self.timeslot = Timeslot
+        self.timeslot = timeslot
         self.capacity = capacity
         self.year = year
         self.section = section
         self.semester = semester
-
-class Time:
-    def __init__(self, time):
-        self.time = time
-
-class Timeslot:
-    def __init__(self, day, time):
-        self.day = day
-        self.time = time
 
 class CourseSchedule:
     def __init__(self):
@@ -50,13 +41,6 @@ class CourseSchedule:
     def get_courses_by_student(self, student):
         # Get a list of courses that a given student is enrolled in
         return [course for course in self.courses if student in course.students]
-    
-    def check_missing(self, course, student_plan):
-        # Return the classes that are missing from the student's plan
-        if course in student_plan:
-            return True
-        else:
-            course.add_course 
     
     def get_courses_by_teacher(self, teacher):
         # Get a list of courses that a given teacher is teaching
