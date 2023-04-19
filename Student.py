57c5136b938e9cd671a78f8436ec12569b02fd17
Eric Weinberg <ericweinberg@To-B106-P4QY19QG0H.local>
2023-04-19 12:29:57 -0500

    comments

--- Student.py
+++ Student.py
@@ -1,34 +1,48 @@
+# Import necessary libraries and modules
 import csv
 from typing import Dict
 from schedule import Course, ScheduledClass, CourseSchedule, Time, TimeSlot
 from people import Teacher, Student
 
+# Define a function to load scheduled classes from a CSV file
 def load_scheduled_classes(csv_path: str, course_schedule: CourseSchedule):
+    # Initialize dictionaries to store instructors and courses
     instructors = {}
     courses = {}
 
+    # Open the CSV file for reading
     with open(csv_path, "r") as file:
+        # Create a CSV reader object
         reader = csv.reader(file)
+        # Read and discard the header row
         header = next(reader)
+        
+        # Loop through the remaining rows in the CSV file
         for row in reader:
+            # Unpack the row into individual variables
             sec_name, title, sec_mthd, wks, sec_start, credits, days, time_start, time_end, room, instructor_id, capacity, semester, year = row
             
-            # Create Instructor object if not already exists
-            instructor_id = instructor_id
+            # Check if the instructor already exists in the instructors dictionary
+            # If not, create a new Teacher object and add it to the dictionary
             if instructor_id not in instructors:
                 instructors[instructor_id] = Teacher(instructor_id, f"Instructor {instructor_id}")
             
-            # Create Course object if not already exists
+            # Check if the course already exists in the courses dictionary
+            # If not, create a new Course object and add it to the dictionary
             course_id = sec_name.split('-')[0]
             if course_id not in courses:
                 courses[course_id] = Course(course_id, title)
             
-            # Create Time and TimeSlot objects
+            # Create Time and TimeSlot objects using the start and end times and days
             time = Time(time_start, time_end)
             timeslot = TimeSlot(days, time)
             
-            # Create CourseClass object and add it to CourseSchedule
-            section=1
-            course_class = ScheduledClass(time, instructors[instructor_id], timeslot, int(capacity), int(year),section, semester)
+            # Create a ScheduledClass object with the necessary information
+            section = 1
+            course_class = ScheduledClass(time, instructors[instructor_id], timeslot, int(capacity), int(year), section, semester)
+            
+            # Add the ScheduledClass object to the CourseSchedule
             course_schedule.add_course(course_class)
+    
+    # Return the updated CourseSchedule object
     return course_schedule
--- load_plans.py
+++ load_plans.py
@@ -1,10 +1,4 @@
-import os
-import csv
-from typing import List
-from people import Person, Student, Teacher
-from schedule import Course,PlannedCourse
-from plans import StudentPlan
-
+# Import necessary libraries and modules
 import os
 import csv
 from typing import List
@@ -12,32 +6,44 @@ from people import Person, Student, Teacher
 from schedule import Course, PlannedCourse
 from plans import StudentPlan
 
+# Define a function to load student plans from a CSV file
 def load_student_plans(csv_path: str) -> List[Student]:
+    # Initialize dictionaries to store student plans, courses, and students
     student_plans = {}
     courses = {}
     students = {}
 
+    # Open the CSV file for reading
     with open(csv_path, "r") as file:
+        # Create a CSV DictReader object
         readCSV = csv.DictReader(file)
         
+        # Loop through the rows in the CSV file
         for row in readCSV:
+            # Get student_id, year, semester, and course_id from the row
             student_id = int(row['student_id'])
             year = int(row['year'])
             semester = row['semester']
             course_id = row['course']
             
+            # Check if the student already exists in the student_plans dictionary
+            # If not, create a new Student object with an associated StudentPlan and add it to the dictionaries
             if student_id not in student_plans:
-                student = Student(student_id, f"Student {student_id}",[])
+                student = Student(student_id, f"Student {student_id}", [])
                 student.add_plan(StudentPlan(student))
                 student_plans[student_id] = student
                 students[student_id] = student
 
+            # Check if the course already exists in the courses dictionary
+            # If not, create a new Course object and add it to the dictionary
             if course_id not in courses:
                 courses[course_id] = Course(course_id, f"Course {course_id}")
             
+            # Create a PlannedCourse object with the course, year, and semester
             planned_class = PlannedCourse(courses[course_id], year, semester)
 
             # Add the planned class to the student's plan
             student_plans[student_id].plans[0].add_planned_class(planned_class)
 
+    # Return a list of Student objects
     return [s for s in students.values()]
